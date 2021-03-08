761f4636bf5cc81664e569a12b59c6ed05e1d04a
Azou <pika.azou@gmail.com>
2021-03-08 18:16:49 +0100

    Change data loading function to generate X and y and change plot_scatter_2d function in main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from sklearn.model_selection import train_test_split
+import csv
 
 # Types de consonnes
 cons_types = ["aa", "ee", "eh", "eu", "ii", "oe", "oh", "oo", "uu", "yy"]
@@ -15,36 +17,58 @@ cons_types = ["aa", "ee", "eh", "eu", "ii", "oe", "oh", "oo", "uu", "yy"]
 files = ["data2.csv", "data3.csv", "data12.csv"]
 
 # Récupération des données
-def get_data(path):
-    fd = open(path)
-    content = fd.read().split("\n")
-    for i in range(len(content)):
-        if len(content[i]) == 0:
-            content.pop(i)
-    temp_data = []
-    for e in content:
-        temp_e = e.split(",")
-        temp_data.append(temp_e)
-    data = []
-    for t in cons_types:
-        temp_e = []
-        for e in temp_data:
-            if e[-1] == t:
-                new_e = []
-                for i in range(len(e) - 1):
-                    new_e.append(float(e[i]))
-                temp_e.append(new_e)
-        data.append(temp_e)
-    return np.array(data)
+def load_dataset(pathname:str):
+    """Load a dataset in csv format.
 
-def plot_scatter_2d(data) :
-    for x in data :
-        plt.scatter(x[:,1], x[:,0], s=20, alpha=0.5)
-    plt.legend(cons_types)
+    Each line of the csv file represents a data from our dataset and each
+    column represents the parameters.
+    The last column corresponds to the label associated with our data.
+
+    Parameters
+    ----------
+    pathname : str
+        The path of the csv file.
+
+    Returns
+    -------
+    data : ndarray
+        All data in the database.
+    labels : ndarray
+        Labels associated with the data.
+    """
+    # check the file format through its extension
+    if pathname[-4:] != '.csv':
+        raise OSError("The dataset must be in csv format")
+    # open the file in read mode
+    with open(pathname, 'r') as csvfile:
+        # create the reader object in order to parse the data file
+        reader = csv.reader(csvfile, delimiter=',')
+        # extract the data and the associated label
+        # (he last column of the file corresponds to the label)
+        data = []
+        labels = []
+        for row in reader:
+            data.append(row[:-1])
+            labels.append(row[-1])
+        # converts Python lists into NumPy matrices
+        # in the case of the list of labels, generate an int id per class
+        data = np.array(data, dtype=float)
+        lookupTable, labels = np.unique(labels, return_inverse=True)
+    # return data with the associated label
+    return data, labels
+
+def plot_scatter_2d(data,labels) :
+    for y in np.unique(labels) :
+        x = data[labels == y]
+        plt.scatter(x[:,1], x[:,0], s=20, alpha=0.5,  label=cons_types[y])
+    plt.legend()
     plt.show()
 
 # Programme principal
 if __name__ == '__main__':
     path = './Data'
-    data = get_data(os.path.join(path, files[0]))
-    plot_scatter_2d(data)
+    X, y = load_dataset(os.path.join(path, files[0]))
+    plot_scatter_2d(X,y)
+
+
+
