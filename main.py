1739057ed39e07d9b423e4d4d2dc71add68134a5
alexcla99 <alex@LAPTOP-EKVPB71K.localdomain>
2021-03-10 23:22:49 +0100

    Classification KPPV et fonction d'affichage de matrice de confusion

--- main.py
+++ main.py
@@ -8,68 +8,67 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
 import csv
 
 # Types de consonnes
-cons_types = ["aa", "ee", "eh", "eu", "ii", "oe", "oh", "oo", "uu", "yy"]
+cons_types = np.array(["aa", "ee", "eh", "eu", "ii", "oe", "oh", "oo", "uu", "yy"])
 
 # fichiers à traiter
-files = ["data2.csv", "data3.csv", "data12.csv"]
+files = np.array(["data2.csv", "data3.csv", "data12.csv"])
 
 # Récupération des données
-def load_dataset(pathname:str):
-    """Load a dataset in csv format.
-
-    Each line of the csv file represents a data from our dataset and each
-    column represents the parameters.
-    The last column corresponds to the label associated with our data.
-
-    Parameters
-    ----------
-    pathname : str
-        The path of the csv file.
-
-    Returns
-    -------
-    data : ndarray
-        All data in the database.
-    labels : ndarray
-        Labels associated with the data.
-    """
-    # check the file format through its extension
+def load_dataset(pathname:str) -> (np.ndarray, np.ndarray):
     if pathname[-4:] != '.csv':
         raise OSError("The dataset must be in csv format")
-    # open the file in read mode
     with open(pathname, 'r') as csvfile:
-        # create the reader object in order to parse the data file
         reader = csv.reader(csvfile, delimiter=',')
-        # extract the data and the associated label
-        # (he last column of the file corresponds to the label)
         data = []
         labels = []
         for row in reader:
             data.append(row[:-1])
             labels.append(row[-1])
-        # converts Python lists into NumPy matrices
-        # in the case of the list of labels, generate an int id per class
         data = np.array(data, dtype=float)
         lookupTable, labels = np.unique(labels, return_inverse=True)
-    # return data with the associated label
     return data, labels
 
-def plot_scatter_2d(data,labels) :
-    for y in np.unique(labels) :
-        x = data[labels == y]
-        plt.scatter(x[:,1], x[:,0], s=20, alpha=0.5,  label=cons_types[y])
-    plt.legend()
+# Affichage des données en 2D
+def plot_scatter_2d(X:np.ndarray, y:np.ndarray) -> None:
+    plt.scatter(X[:,1], X[:,0], s=20, alpha=0.5, c=y)
+    plt.legend(cons_types)
+    plt.show()
+    
+# Affichage d'une matrice de confusion
+def plot_confusion_matrix(y_test:np.ndarray, y_pred:np.ndarray, features:np.ndarray, c_name:str) -> None:
+    plt.imshow(confusion_matrix(y_test, y_pred))
+    plt.xticks(np.arange(len(features)), features, rotation=45)
+    plt.yticks(np.arange(len(features)), features)
+    plt.ylabel("Véritable classe associée")
+    plt.xlabel("Classe prédite")
+    plt.title("Matrice de confusion pour {}".format(c_name))
+    plt.colorbar()
     plt.show()
 
 # Programme principal
 if __name__ == '__main__':
-    path = './Data'
+    path = 'Data'
     X, y = load_dataset(os.path.join(path, files[0]))
-    plot_scatter_2d(X,y)
-    # create training and test sets
+    #plot_scatter_2d(X,y)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
-
-
+    #print("X_train: {}".format(X_train))
+    #print("X_test: {}".format(X_test))
+    #print("y_train: {}".format(y_train))
+    #print("y_test: {}".format(y_test))
+    
+    # Classification KPPV (KNN)
+    n_neighbors = len(cons_types)
+    n_test_items = len(X_test)
+    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+    knn.fit(X_train, y_train)
+    knn_predictions = knn.predict(X_test)
+    knn_good_predictions = np.sum(y_test == knn_predictions)
+    print("Taux de reconnaissance de KPPV  : {}/{}".format(knn_good_predictions, n_test_items))
+    knn_score = knn_good_predictions / n_test_items
+    print("Score de précision associé : {}".format(knn_score))
+    plot_confusion_matrix(y_test, knn_predictions, cons_types, "KPPV")
