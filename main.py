2e54bcdc06b5facf46e4375684efe29381a2e58a
alexcla99 <alex@LAPTOP-EKVPB71K.localdomain>
2021-03-10 23:39:51 +0100

    Améliorations mineures

--- main.py
+++ main.py
@@ -40,35 +40,36 @@ def plot_scatter_2d(X:np.ndarray, y:np.ndarray) -> None:
     plt.show()
     
 # Affichage d'une matrice de confusion
-def plot_confusion_matrix(y_test:np.ndarray, y_pred:np.ndarray, features:np.ndarray, c_name:str) -> None:
+def plot_confusion_matrix(y_test:np.ndarray, y_pred:np.ndarray, features:np.ndarray, c_name:str, f_name:str) -> None:
     plt.imshow(confusion_matrix(y_test, y_pred))
     plt.xticks(np.arange(len(features)), features, rotation=45)
     plt.yticks(np.arange(len(features)), features)
     plt.ylabel("Véritable classe associée")
     plt.xlabel("Classe prédite")
-    plt.title("Matrice de confusion pour {}".format(c_name))
+    plt.title("Matrice de confusion pour {} ({})".format(c_name, f_name))
     plt.colorbar()
     plt.show()
 
 # Programme principal
 if __name__ == '__main__':
     path = 'Data'
-    X, y = load_dataset(os.path.join(path, files[0]))
-    #plot_scatter_2d(X,y)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
-    #print("X_train: {}".format(X_train))
-    #print("X_test: {}".format(X_test))
-    #print("y_train: {}".format(y_train))
-    #print("y_test: {}".format(y_test))
-    
-    # Classification KPPV (KNN)
-    n_neighbors = len(cons_types)
-    n_test_items = len(X_test)
-    knn = KNeighborsClassifier(n_neighbors=n_neighbors)
-    knn.fit(X_train, y_train)
-    knn_predictions = knn.predict(X_test)
-    knn_good_predictions = np.sum(y_test == knn_predictions)
-    print("Taux de reconnaissance de KPPV  : {}/{}".format(knn_good_predictions, n_test_items))
-    knn_score = knn_good_predictions / n_test_items
-    print("Score de précision associé : {}".format(knn_score))
-    plot_confusion_matrix(y_test, knn_predictions, cons_types, "KPPV")
+    for f in range(len(files)):
+        X, y = load_dataset(os.path.join(path, files[f]))
+        #plot_scatter_2d(X,y)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+        #print("X_train: {}".format(X_train))
+        #print("X_test: {}".format(X_test))
+        #print("y_train: {}".format(y_train))
+        #print("y_test: {}".format(y_test))
+
+        # Classification KPPV (KNN)
+        n_neighbors = len(cons_types)
+        n_test_items = len(X_test)
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors)
+        knn.fit(X_train, y_train)
+        knn_predictions = knn.predict(X_test)
+        knn_good_predictions = np.sum(y_test == knn_predictions)
+        plot_confusion_matrix(y_test, knn_predictions, cons_types, "KPPV", files[f])
+        print("Taux de reconnaissance ({}) : {}/{}".format(files[f], knn_good_predictions, n_test_items))
+        knn_score = knn_good_predictions / n_test_items
+        print("Score de précision associé ({}) : {}".format(files[f], knn_score))
