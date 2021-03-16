78543c8b4d73502dec0705254d8a01b0e4331a63
alexcla99 <alex@LAPTOP-EKVPB71K.localdomain>
2021-03-16 18:28:37 +0100

    NE PAS TENIR COMPTE DES DEUX DERNIERS COMMITS - Finalisation du code et captures d'écran

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 import csv
+import time
 
 # Types de consonnes
 cons_types = np.array(["aa", "ee", "eh", "eu", "ii", "oe", "oh", "oo", "uu", "yy"])
@@ -43,46 +44,51 @@ def plot_scatter_2d(X:np.ndarray, y:np.ndarray) -> None:
     plt.show()
     
 # Affichage d'une matrice de confusion
-def plot_confusion_matrix(y_test:np.ndarray, y_pred:np.ndarray, features:np.ndarray, c_name:str, f_name:str, distribution:float, rr1:int, rr2:int) -> None:
+def plot_confusion_matrix(i:int, y_test:np.ndarray, y_pred:np.ndarray, features:np.ndarray, 
+                          distribution:float, rr1:int, rr2:int, exec_time:float) -> None:
     cm = confusion_matrix(y_test, y_pred)
-    plt.figure()
-    plt.imshow(cm)
-    plt.xticks(np.arange(len(features)), features)
-    plt.yticks(np.arange(len(features)), features)
+    ax = plt.subplot(3, 3, i + 1) # len(distribs) = 9 donc 3 x 3
+    ax.imshow(cm)
+    ax.set_xticks(np.arange(len(features)))
+    ax.set_yticks(np.arange(len(features)))
+    ax.set_xticklabels(features)
+    ax.set_yticklabels(features)
     for i in range(len(features)):
         for j in range(len(features)):
-            plt.text(i, j, str(cm[i][j]), size='small', ha='center', va='center')
-    plt.ylabel("Véritable classe associée (y_gold)")
-    plt.xlabel("Classe prédite (y_pred)")
-    plt.suptitle("Matrice de confusion du classifieur {}".format(c_name))
-    plt.title("{} | d={}/{} | rr={}/{}".format(f_name, int(np.around(distribution*100.,0)), int(np.around((1.-distribution)*100.,0)), rr1, rr2), fontsize=10)
-    plt.colorbar()
-    plt.show()
-    
-# Affichage d'un graphe d'évolution du score de précision
-def plot_accur_evolution(scores:np.ndarray, distribution:np.array, dims:np.ndarray) -> None:
+            ax.text(i, j, str(cm[i][j]), size='small', ha='center', va='center')
+    ax.set_ylabel("Véritable classe associée (y_gold)", fontsize=8)
+    ax.set_xlabel("Classe prédite (y_pred)", fontsize=8)
+    ax.set_title("d={}/{} | rr={}/{} | t={:0.4f}s".format(int(np.around(distribution*100.,0)), 
+                                                     int(np.around((1.-distribution)*100.,0)), rr1, rr2, exec_time), 
+                                                     fontsize=10)
+
+# Affichage d'un graphe d'évolution selon la distribution des données de test
+def plot_accur_evolution(scores:np.ndarray, distribution:np.array, dims:np.ndarray, y_label:str) -> None:
     plt.figure()
     for e in scores:
         plt.plot(e)
     plt.xticks(np.arange(len(distribution)), [np.around(e, 1) for e in distribution])
-    plt.ylabel("Score de précision")
+    plt.ylabel(y_label)
     plt.xlabel("Distribution des données de test")
     plt.legend(dims, title="Dimensions\ndes données")
-    plt.title("Evolution du score de précision pour KPPV")
+    plt.title("Evolution du {} pour KPPV".format(y_label.lower()))
     plt.show()
 
 # Programme principal
 if __name__ == '__main__':
     path = 'Data'
     knn_scores = [] 
+    exec_time_t = []
     dimensions = []
     for f in range(len(files)):
         file_knn_scores = []
+        file_exec_time = []
         X, y = load_dataset(os.path.join(path, files[f]))
         #plot_scatter_2d(X,y)
         dimensions.append(X.shape[-1])
-        print("Avec {} dimensions :".format(dimensions[f]))
+        plt.figure(figsize=(12., 13.))
         for d in range(len(distribs)):
+            start = time.perf_counter()
             X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=distribs[d], random_state=0)
             #print("X_train: {}".format(X_train))
             #print("X_test: {}".format(X_test))
@@ -96,13 +102,19 @@ if __name__ == '__main__':
             knn.fit(X_train, y_train)
             knn_predictions = knn.predict(X_test)
             knn_good_predictions = np.sum(y_test == knn_predictions)
-            plot_confusion_matrix(y_test, knn_predictions, cons_types, "KPPV", files[f], distribs[d], knn_good_predictions, n_test_items)
+            stop = time.perf_counter()
+            plot_confusion_matrix(d, y_test, knn_predictions, cons_types, distribs[d], 
+                                  knn_good_predictions, n_test_items, stop - start)
 
             # Récupération des scores
             file_knn_score = knn_good_predictions / n_test_items
             file_knn_scores.append(file_knn_score)
+            file_exec_time.append(stop - start)
+        plt.suptitle("Matrices de confusion pour KPPV avec {} dimensions :".format(dimensions[f]), y=.92)
+        plt.show()
         knn_scores.append(file_knn_scores)
-        print("---------------------------------------------------------")
+        exec_time_t.append(file_exec_time)
 
     # Courbe d'évolution de la précision pour le classifieur KPPV
-    plot_accur_evolution(knn_scores, distribs, dimensions)
+    plot_accur_evolution(knn_scores, distribs, dimensions, "Score de précision")
+    plot_accur_evolution(exec_time_t, distribs, dimensions, "Temps d'exécution")
